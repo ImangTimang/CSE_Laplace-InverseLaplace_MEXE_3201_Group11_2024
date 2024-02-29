59e875f27f8a9bcd624243e42b7025c5cbbd715a
Ralph Neil Maranan <157492494+RNMaranan@users.noreply.github.com>
2024-02-29 21:07:28 +0800

    Add files via upload

---
+++ Assignment 1/lablab1.m
@@ -0,0 +1,19 @@
+% Clear
+clear
+clc
+close all
+
+syms s t
+%% Assignment 1 - Laplace
+
+a = (3-exp(-3*t)+5*sin(2*t))
+A = laplace(a)
+pretty (A)
+
+b = (3+12*t+42*t^3-3*exp(2*t))
+B = laplace(b)
+pretty (B)
+
+c = ((t+1)*(t+2))
+C = laplace(c)
+pretty (C)
